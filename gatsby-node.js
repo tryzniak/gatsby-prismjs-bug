726af6424ebd2b17b6d260d5a725d6f0f15484e6
Anton Harniakou <anton.harniakou@gmail.com>
2018-08-15 20:44:21 +0300

    first commit

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve(`./src/templates/blog-post.js`)
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(limit: 1000) {
+              edges {
+                node {
+                  frontmatter {
+                    path
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        // Create blog posts pages.
+        _.each(result.data.allMarkdownRemark.edges, edge => {
+          createPage({
+            path: edge.node.frontmatter.path,
+            component: blogPost,
+          })
+        })
+      })
+    )
+  })
+}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { css } from "emotion"
+import { graphql } from "gatsby"
+import { rhythm, scale } from "../utils/typography"
+
+import Layout from "../components/layout"
+
+const postContainer = css`
+  max-width: ${rhythm(30)};
+  margin: auto;
+`
+
+const postDate = css`
+  ${scale(-1 / 5)};
+  display: block;
+  marginbottom: ${rhythm(1)};
+  margintop: ${rhythm(-1)};
+`
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+
+    return (
+      <Layout pageTitle={post.frontmatter.title}>
+        <div className={postContainer}>
+          <h1>{post.frontmatter.title}</h1>
+          <p className={postDate}>{post.frontmatter.date}</p>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr />
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      id
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        path
+        description
+      }
+    }
+  }
+`
